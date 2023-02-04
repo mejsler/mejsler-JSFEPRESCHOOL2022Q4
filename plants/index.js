887d1710a28c935a5eecc366a6ac1d509c07c874
mejsler <mejsler@pm.me>
2023-02-04 08:05:41 +0500

    feat: add contacts functionality

--- plants/index.js
+++ plants/index.js
@@ -3,19 +3,16 @@ const menuBtn = document.querySelector('#menuBtn');
 const serviceBtns = document.querySelector('.service__nav__buttons');
 const serviceList = document.querySelector('.service__list');
 const priceItems = document.querySelectorAll('.prices__accordion__item');
+const contacts = document.querySelector('.contacts__select');
+const contactsList = document.querySelector('.contacts__list');
 
-menuBtn.addEventListener(
-  'click',
-  () => {
-    menu.classList.toggle('open');
-    menuBtn.classList.toggle('open');
-  },
-  false
-);
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+});
 
 menu.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.tagName === 'A') {
+  if (event.target.tagName === 'A') {
     menu.classList.remove('open');
     menuBtn.classList.remove('open');
   }
@@ -63,6 +60,51 @@ priceItems.forEach((item) =>
     });
   })
 );
+const info = [
+  {
+    0: 'Canandaigua, NY',
+    1: '+1	585	393 0001',
+    2: '151 Charlotte Street',
+    3: '5853930001'
+  },
+  {
+    0: 'New York City',
+    1: '+1	212	456 0002',
+    2: '9 East 91st Street',
+    3: '2124560002'
+  },
+  {
+    0: 'Yonkers, NY',
+    1: '+1	914	678 0003',
+    2: '511 Warburton Ave',
+    3: '9146780003'
+  },
+  {
+    0: 'Sherrill, NY',
+    1: '+1	315	908 0004',
+    2: '14 WEST Noyes BLVD',
+    3: '3159080004'
+  },
+];
+
+contacts.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName !== 'A' || target.classList.contains('.contacts__info')) {
+    contactsList.classList.toggle('open');
+  }
+  if (target.tagName === 'LI') {
+    const table = contacts.querySelectorAll('.data');
+    const city = target.innerHTML;
+    contactsList.classList.remove('open');
+    contacts.querySelector('.selected .kek').innerHTML = city;
+    contacts.querySelector('.contacts__info').classList.add('open');
+    info.forEach((e) => {
+      if (e[0] === city) {
+        table.forEach((d, i) => i === 3 ? d.setAttribute("href", `tel:+1${e[i]}`) : d.innerHTML = e[i]);
+      }
+    });
+  }
+});
 
 // console.log("✓ 1. Вёрстка валидная +10 \n✓ 2. Вёрстка семантическая +20\n✓ 3. Вёрстка соответствует макету +48\n✓ 4. Требования к css + 12\n✓ 5. Интерактивность, реализуемая через css +20\n Самоценка за задание: 110 баллов")
 // console.log(
