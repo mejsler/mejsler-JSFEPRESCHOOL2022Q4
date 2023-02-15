390ce1695408baf75efbbbef410a884ebf1133f3
mejsler <mejsler@pm.me>
2023-02-15 21:53:07 +0500

    feat: add better audioplayer

--- momentum/modules/playlist.js
+++ momentum/modules/playlist.js
@@ -1,27 +1,22 @@
 export const playlist = [
   {
     title: 'Done',
-    src: './assets/sounds/done.mp3',
+    src: 'assets/sounds/done.mp3',
     duration: '00:00',
   },
   {
     title: 'Aqua Caelestis',
-    src: './assets/sounds/AquaCaelestis.mp3',
+    src: 'assets/sounds/AquaCaelestis.mp3',
     duration: '00:39',
   },
   {
     title: 'River Flows In You',
-    src: './assets/sounds/RiverFlowsInYou.mp3',
+    src: 'assets/sounds/RiverFlowsInYou.mp3',
     duration: '01:37',
   },
   {
     title: 'Ennio Morricone',
-    src: './assets/sounds/EnnioMorricone.mp3',
+    src: 'assets/sounds/EnnioMorricone.mp3',
     duration: '01:37',
-  },
-  {
-    title: 'Summer Wind',
-    src: './assets/sounds/SummerWind.mp3',
-    duration: '01:50',
-  },
+  }
 ];
